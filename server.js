8c15ab96a6c78066cf2c67a0987ce59c4a03fb39
reshmaps095 <reshmaps095@gmail.com>
2022-03-28 14:11:40 +0530

    test

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/BENIXTEST'));
+app.use(express.static('./dist/benix-test'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/BENIXTEST/'}),
+    res.sendFile('index.html', {root: 'dist/benix-test/'}),
 );
 
 // Start the app by listening on the default Heroku port
